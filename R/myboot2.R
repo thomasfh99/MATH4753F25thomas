c5d22dad27c9efdb8ba426ec45669dbd8a36e46c
thomasfh99 <thomas.f.hernandez-1@ou.edu>
2025-10-31 12:08:21 -0500

    added spruce data set and myboot2 function

---
+++ R/myboot2.R
@@ -0,0 +1,45 @@
+#' myboot2
+#'
+#' @param iter number of iterations
+#' @param x sample size
+#' @param fun the function to be used (eg.: mean, variance)
+#' @param alpha Confidence level (1 - alpha = confidence level)
+#' @param cx a constant value
+#' @param ... Ability to include extra parameters
+#' @importFrom graphics segments text
+#' @importFrom stats quantile
+#'
+#' @returns A bootstrap function with a histogram that has confidence intervals and other sample statistics
+#' @export
+#'
+#' @examples
+#' \dontrun{myboot2(iter=10000, x=sam, fun="mean", alpha=0.05, xlab="mean", col="#0f20db")}
+myboot2 = function(iter=10000,x,fun="mean",alpha=0.05,cx=1.5,...){  #Notice where the ... is repeated in the code
+  n=length(x)   #sample size
+
+  y=sample(x,n*iter,replace=TRUE)
+  rs.mat=matrix(y,nrow=n,ncol=iter,byrow=TRUE)
+  xstat=apply(rs.mat,2,fun) # xstat is a vector and will have iter values in it
+  ci=quantile(xstat,c(alpha/2,1-alpha/2))# Nice way to form a confidence interval
+  # A histogram follows
+  # The object para will contain the parameters used to make the histogram
+  para=hist(xstat,freq=FALSE,las=1,
+            main=paste("Histogram of Bootstrap sample statistics","\n","alpha=",alpha," iter=",iter,sep=""),
+            ...)
+
+  #mat will be a matrix that contains the data, this is done so that I can use apply()
+  mat=matrix(x,nrow=length(x),ncol=1,byrow=TRUE)
+
+  #pte is the point estimate
+  #This uses whatever fun is
+  pte=apply(mat,2,fun)
+  abline(v=pte,lwd=3,col="Black")# Vertical line
+  segments(ci[1],0,ci[2],0,lwd=4)      #Make the segment for the ci
+  text(ci[1],0,paste("(",round(ci[1],2),sep=""),col="Red",cex=cx)
+  text(ci[2],0,paste(round(ci[2],2),")",sep=""),col="Red",cex=cx)
+
+  # plot the point estimate 1/2 way up the density
+  text(pte,max(para$density)/2,round(pte,2),cex=cx)
+
+  return(list(ci=ci,fun=fun,x=x))# Some output to use if necessary
+}
---
+++ R/spruce.R
@@ -0,0 +1,12 @@
+#' SPRUCE data set.
+#'
+#' A data set from MS
+#'
+#' @format A data frame with 36 rows and 2 variables:
+#' \describe{
+#'   \item{BHDiameter}{breast height diameter in cm}
+#'   \item{Height}{height in m}
+#'   ...
+#' }
+#' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
+"spruce"
