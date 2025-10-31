ebea634397daf1a3f1f91c34163f87cd0e4f153a
thomasfh99 <thomas.f.hernandez-1@ou.edu>
2025-10-31 12:44:00 -0500

    added FIREDAM.csv data set

---
+++ R/fire.R
@@ -0,0 +1,12 @@
+#' FIRE data set.
+#'
+#' A data set from MS
+#'
+#' @format A data frame with 15 rows and 2 variables:
+#' \describe{
+#'   \item{DISTANCE}{length in cm}
+#'   \item{DAMAGE}{Dollars damage}
+#'   ...
+#' }
+#' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
+"fire"
