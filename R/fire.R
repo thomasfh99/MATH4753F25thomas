6795841311cd20291aaa4eab8b5967de5451c99d
thomasfh99 <thomas.f.hernandez-1@ou.edu>
2025-12-03 17:59:24 -0600

    updated a few things

--- R/fire.R
+++ R/fire.R
@@ -1,12 +1,11 @@
 #' FIRE data set.
 #'
-#' A data set from MS
+#' A data set from MS that shows data from fire damage incidents.
 #'
 #' @format A data frame with 15 rows and 2 variables:
 #' \describe{
-#'   \item{DISTANCE}{length in cm}
-#'   \item{DAMAGE}{Dollars damage}
-#'   ...
+#'   \item{DISTANCE}{distance from fire station to fire, in miles}
+#'   \item{DAMAGE}{Thousands of dollars damage}
 #' }
 #' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
 "fire"
--- R/spruce.R
+++ R/spruce.R
@@ -1,12 +1,11 @@
 #' SPRUCE data set.
 #'
-#' A data set from MS
+#' A data set from MS that measures different components of spruce trees.
 #'
 #' @format A data frame with 36 rows and 2 variables:
 #' \describe{
 #'   \item{BHDiameter}{breast height diameter in cm}
 #'   \item{Height}{height in m}
-#'   ...
 #' }
 #' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
 "spruce"
