70cf756430d2b5354f3cd9fdbfb85e53cab6da36
thomasfh99 <thomas.f.hernandez-1@ou.edu>
2025-12-03 13:03:38 -0600

    added the ddt data set

---
+++ R/ddt.R
@@ -0,0 +1,15 @@
+#' DDT data set.
+#'
+#' A data set collected by the US forces from the Tennessee river and its tributaries
+#'
+#' @format A data frame with 144 rows and 6 variables:
+#' \describe{
+#'   \item{RIVER}{name of river}
+#'   \item{MILE}{mile marker along the river}
+#'   \item{SPECIES}{type of fish}
+#'   \item{LENGTH}{length in cm}
+#'   \item{WEIGHT}{weight of the fish, in gms}
+#'   \item{DDT}{value of DDT in ppm}
+#' }
+#' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
+"ddt"
